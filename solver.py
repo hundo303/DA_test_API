eaa984b3ea79b8ed238284881e12500773eda6fa
hundo <44696693+hundo303@users.noreply.github.com>
2022-02-08 21:19:42 +0900

    漢数字からアラビア数字への変換ができた

--- solver.py
+++ solver.py
@@ -13,30 +13,57 @@ def ar_to_ja(input_ar: int):
     if input_ar == 0:
         return '零'
 
-    return_str = ''
+    ja_str = ''
     for i in range(len(ar_re)):
         if ar_re[i] == '0':
             if i % 4 == 0:
-                return_str += q.get()
+                ja_str += q.get()
             continue
 
         if (i + 3) % 4 == 0:
-            return_str += '拾'
+            ja_str += '拾'
         elif (i + 2) % 4 == 0:
-            return_str += '百'
+            ja_str += '百'
         elif (i + 1) % 4 == 0:
-            return_str += '千'
+            ja_str += '千'
         elif i % 4 == 0 and i != 0:
-            return_str += q.get()
+            ja_str += q.get()
 
-        return_str += ar_ja_dict[ar_re[i]]
+        ja_str += ar_ja_dict[ar_re[i]]
 
-    return return_str[::-1]
+    return ja_str[::-1]
 
 
 def ja_to_ar(input_ja: str):
-    pass
+    ja_ar_dict = {'壱': 1, '弐': 2, '参': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9}
+    ar_n = 0
+    tmp_n = 0
+    for i in range(1, len(input_ja)):
+        if input_ja[i] == '拾':
+            tmp_n += ja_ar_dict[input_ja[i - 1]] * 10
+        elif input_ja[i] == '百':
+            tmp_n += ja_ar_dict[input_ja[i - 1]] * 100
+        elif input_ja[i] == '千':
+            tmp_n += ja_ar_dict[input_ja[i - 1]] * 1000
+
+        elif input_ja[i] == '兆':
+            tmp_n += ja_ar_dict.get(input_ja[i - 1], 0)
+            ar_n += tmp_n * 1000000000000
+            tmp_n = 0
+        elif input_ja[i] == '億':
+            tmp_n += ja_ar_dict.get(input_ja[i - 1], 0)
+            ar_n += tmp_n * 100000000
+            tmp_n = 0
+        elif input_ja[i] == '万':
+            tmp_n += ja_ar_dict.get(input_ja[i - 1], 0)
+            ar_n += tmp_n * 10000
+            tmp_n = 0
+
+    ar_n += tmp_n
+    ar_n += ja_ar_dict.get(input_ja[-1], 0)
+
+    return ar_n
 
 
 if __name__ == '__main__':
-    print(ar_to_ja(9999999999999999))
+    pass
